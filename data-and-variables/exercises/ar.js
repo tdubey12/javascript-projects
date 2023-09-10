4b7da4933be4e000e3431291fb52ada9ffc58afb
Teena Dubey <avidubey2k5@gmail.com>
2023-09-10 11:36:34 -0500

    area of ractangle

---
+++ data-and-variables/exercises/ar.js
@@ -0,0 +1,6 @@
+const input = require('readline-sync');
+let length = input.question("enter length");
+let height = input.question("enter height");
+let area =length*height;
+console.log("area"+area);
+
