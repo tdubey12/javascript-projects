6b75d29a76fc485a7539d43f2cd3564429c4f644
Teena Dubey <avidubey2k5@gmail.com>
2023-09-13 20:46:18 -0500

    booleans-and-conditionals

--- booleans-and-conditionals/exercises/part-3.js
+++ booleans-and-conditionals/exercises/part-3.js
@@ -6,7 +6,7 @@ let engineTemperature = 1200;
 
 a) If fuelLevel is above 20000 AND engineTemperature is at or below 2500, print "Full tank. Engines good."
 
-b) If fuelLevel is above 10000 AND engineTemperature is at or below 2500, print "Fuel level above 50%.  Engines good."
+b) If fuelLevel is above 10000 AND engineTemperature is at or below 2500, print2500
 
 c) If fuelLevel is above 5000 AND engineTemperature is at or below 2500, print "Fuel level above 25%. Engines good."
 
@@ -17,8 +17,43 @@ e) If fuelLevel is below 1000 OR engineTemperature is above 3500 OR engineIndica
 f) Otherwise, print "Fuel and engine status pending..." */
 
 // Code 5a - 5f here:
+if(fuelLevel < 1000 || engineTemperature > 3500 || engineIndicatorLight ==="red blinking")
+{
+    console.log("ENGINE FAILURE IMMINENT!" );
+}
+else if(fuelLevel<= 5000 || engineTemperature >2500)
+{
+  console.log( "Check fuel level. Engines running hot.") ; 
+}
+else if(fuelLevel >20000 && engineTemperature <=2500)
+{
+    console.log("Full tank. Engines good.");
+}
+else if (fuelLevel >10000 && engineTemperature <=2500)
+{
+    console.log(2500);
+}
+else if (fuelLevel >5000 && engineTemperature <=2500)
+{
+    console.log("Fuel level above 25%. Engines good." );
+}
+else{
+    console.log("Fuel and engine status pending...");
+}
+
+
+
+
 
 // 6) a) Create the variable commandOverride, and set it to be true or false. If commandOverride is false, then the shuttle should only launch if the fuel and engine check are OK. If commandOverride is true, then the shuttle will launch regardless of the fuel and engine status.
+let commandOverride =true;
 
 /* 6) b) Code the following if/else check:
 If fuelLevel is above 20000 AND engineIndicatorLight is NOT red blinking OR commandOverride is true print "Cleared to launch!" Else print "Launch scrubbed!" */
+
+if(fuelLevel > 20000 && engineIndicatorLight !=="red blinking" ||commandOverride) {
+    console.log("cleared to launch!")
+}else{
+    console.log( "Launch scrubbed!")
+}
+
