8cdccc039012669b2a3aa617952b8f24789ead65
Teena Dubey <avidubey2k5@gmail.com>
2023-09-15 21:28:23 -0500

    exercises on array

--- arrays/exercises/part-five-arrays.js
+++ arrays/exercises/part-five-arrays.js
@@ -2,10 +2,18 @@ let str = 'In space, no one can hear you code.';
 let arr = ['B', 'n', 'n', 5];
 
 //1) Use the split method on the string to identify the purpose of the parameter inside the ().
-
+let splitstr =str.split(' ');
+console.log(splitstr);
 //2) Use the join method on the array to identify the purpose of the parameter inside the ().
-
+let joinarr =arr.join('-');
+console.log(joinarr);
 //3) Do split or join change the original string/array?
-
+console.log(str);
+console.log(arr);
 //4) We can take a comma-separated string and convert it into a modifiable array. Try it! Alphabetize the cargoHold string, and then combine the contents into a new string.
 let cargoHold = "water,space suits,food,plasma sword,batteries";
+let cargoholdsplit =cargoHold.split(',');
+console.log(cargoholdsplit);
+ cargoholdsplit[3]="i am bad";
+ console.log(cargoholdsplit);
+ 
--- arrays/exercises/part-four-arrays.js
+++ arrays/exercises/part-four-arrays.js
@@ -4,7 +4,20 @@ let holdCabinet2 = ['orange drink', 'nerf toys', 'camera', 42, 'parsnip'];
 //Explore the methods concat, slice, reverse, and sort to determine which ones alter the original array.
 
 //1) Print the result of using concat on the two arrays. Does concat alter the original arrays? Verify this by printing holdCabinet1 after using the method.
+let holdCabinet =[];
+holdCabinet =holdCabinet1.concat(holdCabinet2);
+holdCabinet1.concat(holdCabinet2);
+console.log(holdCabinet1);
+console.log(holdCabinet);
 
 //2) Print a slice of two elements from each array. Does slice alter the original arrays?
-
+let sliceCabinet =holdCabinet1.slice(2);
+console.log(sliceCabinet);
+console.log(holdCabinet1);
 //3) reverse the first array, and sort the second. What is the difference between these two methods? Do the methods alter the original arrays?
+let reverseCabinet =holdCabinet1.reverse();
+console.log(reverseCabinet);
+console.log(holdCabinet1);
+let sortcabinet =holdCabinet2.sort();
+console.log(sortcabinet);
+console.log(holdCabinet2);
