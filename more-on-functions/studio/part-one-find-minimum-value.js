85473af1bd9cdc6a6ecd98c11dcf8de0e7ef1e93
Teena Dubey <avidubey2k5@gmail.com>
2023-09-30 08:20:35 -0500

    morefunctions

--- more-on-functions/studio/part-one-find-minimum-value.js
+++ more-on-functions/studio/part-one-find-minimum-value.js
@@ -4,7 +4,21 @@
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+function minValueArray(myArray){
+    let minValue = myArray[0];
+    for(let i=1;i<myArray.length;i++){
+        if(minValue>myArray[i]){
+            minValue=myArray[i];
+        }
+    }
+    return minValue
+   
+    
+   }
+   
 
 //Using one of the test arrays as the argument, call your function inside the console.log statement below.
 
-console.log(/* your code here */);
+console.log(minValueArray(nums1));
+console.log(minValueArray(nums2));
+console.log(minValueArray(nums3));
