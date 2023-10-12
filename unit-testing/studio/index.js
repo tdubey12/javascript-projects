ebc978ed2f23d8a9dd6d3db22ebdac9e80becdbc
Teena Dubey <avidubey2k5@gmail.com>
2023-10-11 20:41:02 -0500

    unittest

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,34 @@
 
 let launchcode = {
-  
+    organization: "nonprofit",
+    executiveDirector: "Jeff",
+    percentageCoolEmployees: 100,
+    programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+    launchOutput:function(numberparam){
+        if(numberparam %2 ==0 && numberparam %5==0 && numberparam %3==0){
+            return 'LaunchCode Rocks!';
+        
+        } else if(numberparam %2==0  && numberparam %5==0){
+            return 'Launch Rocks! (CRASH!!!!)';
+        } else if(numberparam %3==0  && numberparam %5==0){
+            return 'Code Rocks!';
+        } else if(numberparam %2 ==0 && numberparam %3==0){
+            return 'LaunchCode!';            
+        } else if(numberparam %2==0){
+            return 'Launch!';
+        } else if(numberparam %3==0){
+            return 'Code!';
+        } else if(numberparam %5==0){
+            return 'Rocks!';
+        
+        } else if(numberparam %2 !=0 && numberparam %5!=0 && numberparam %3!=0){
+            return "Rutabagas! That doesn't work.";
+           
+        }
+
+    }
 }
+
 
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,64 @@
 // launchcode.test.js code:
 const launchcode = require('../index.js');
-
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
+  test("checking organization",()=>{
+    expect(launchcode.organization).toBe("nonprofit");
+  })
   
-});
+  test("checking executive director",()=>{
+    expect(launchcode.executiveDirector).toBe("Jeff");
+  })
+ 
+  test("checking the value of percentageCoolEmployees",()=>{
+    expect(launchcode.percentageCoolEmployees).toBe(100);
+  } )
+  test("checking the program offered",()=>{
+      expect(launchcode.programsOffered[0]).toBe("Web Development");
+    }
+  )
+    test("checking the program offered",()=>{
+      expect(launchcode.programsOffered[1]).toBe("Data Analysis");
+    }
+  ) 
+    test("checking the program offered",()=>{
+      expect(launchcode.programsOffered[2]).toBe("Liftoff");     
+    })
+    test("checking the program offered size",()=>{
+      expect(launchcode.programsOffered.length).toBe(3);     
+    })
+  test("checking number divisible by 2",()=>{
+     expect(launchcode.launchOutput(4)).toBe('Launch!');
+    }
+  )
+    test("checking number divisible by 3",()=>{
+      expect(launchcode.launchOutput(9)).toBe('Code!');
+     }    
+    )
+    test("checking number divisible by 5",()=>{
+      expect(launchcode.launchOutput(25)).toBe('Rocks!');
+     }    
+    )
+    test("checking number divisible by 2&&3",()=>{
+      expect(launchcode.launchOutput(6)).toBe('LaunchCode!');
+     }    
+    )
+    test("checking number divisible by 3&&5",()=>{
+      expect(launchcode.launchOutput(15)).toBe('Code Rocks!');
+     }    
+    )
+    test("checking number divisible by 2&&5",()=>{
+      expect(launchcode.launchOutput(10)).toBe('Launch Rocks! (CRASH!!!!)');
+     }    
+    )
+    test("checking number divisible by 2,3&&5",()=>{
+      expect(launchcode.launchOutput(30)).toBe('LaunchCode Rocks!');
+     }    
+    )
+    test("checking number not divisible by 2,3&&5",()=>{
+      expect(launchcode.launchOutput(17)).toBe("Rutabagas! That doesn't work.");
+     }    
+    )
+  }
+  )
