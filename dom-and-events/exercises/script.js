2edeb929817e79fa6cc1a17768fd7dae49886239
Teena Dubey <avidubey2k5@gmail.com>
2023-10-28 16:09:59 -0500

    dom and events

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,22 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+     });
+     missionAbort.addEventListener("mouseover", event =>{
+        event.target.style.backgroundColor ="red";
+     });
+     missionAbort.addEventListener("mouseout", event =>{
+        event.target.style.backgroundColor ="";
+     });
+    missionAbort.addEventListener('click', event => {
+        let abort=window.confirm("Are you sure you want to abort the mission?");
+        if (abort){
+            paragraph.innerHTML ="Mission aborted! Space shuttle returning home";
+        }
+     });
+     
 }
 
 window.addEventListener("load", init);
